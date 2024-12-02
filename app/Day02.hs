42d110cdf4ea626ba4eb071ed1ad71f1c7b717d9
Patrick Herrmann <patrick.herrmann@classdojo.com>
2024-12-02 18:24:08 +0000

    Implement day 02 part 1

--- app/Day02.hs
+++ app/Day02.hs
@@ -1,7 +1,17 @@
 module Day02 where
 
 part1 :: String -> String
-part1 _ = "Day 02a not implemented yet"
+part1 = show . length . filter safe . map diffs . parseReports
+  where
+    diffs r = zipWith subtract r (tail r)
+    safe r' = all safeInc r' || all safeDec r'
+    safeInc dl = dl >= 1 && dl <= 3
+    safeDec dl = dl <= -1 && dl >= -3
 
 part2 :: String -> String
 part2 _ = "Day 02b not implemented yet"
+
+parseReports :: String -> [[Int]]
+parseReports = map report . lines
+  where
+    report = map read . words
