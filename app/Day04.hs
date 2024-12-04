d437adec5fae60dc5cea497fc1b1e8aa9f6adac2
Patrick Herrmann <patrick.herrmann@classdojo.com>
2024-12-04 07:28:40 +0000

    Implement day 04 part 1

--- app/Day04.hs
+++ app/Day04.hs
@@ -1,7 +1,20 @@
 module Day04 where
 
+import Data.List
+
 part1 :: String -> String
-part1 _ = "Day 04a not implemented yet"
+part1 input = show . sum $ map (\p -> search "XMAS" p + search "SAMX" p) (paths grid)
+  where
+    grid = lines input
+    paths g = g ++ transpose g ++ tlbr g ++ trbl g
+    tlbr g = transpose (zipWith drop [0..] g) ++ transpose (zipWith drop [1..] (transpose g))
+    trbl g = tlbr (reverse g)
+
+search :: String -> String -> Int
+search needle haystack
+  | null haystack = 0
+  | needle `isPrefixOf` haystack = 1 + search needle (drop (length needle) haystack)
+  | otherwise = search needle (tail haystack)
 
 part2 :: String -> String
 part2 _ = "Day 04b not implemented yet"
