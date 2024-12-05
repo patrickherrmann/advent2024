887a7206c3dc6eb687d0ffd9be7e1f3753e7a4f2
Patrick Herrmann <patrick.herrmann@classdojo.com>
2024-12-05 16:35:43 +0000

    Finish part 2 of day 04

--- app/Day04.hs
+++ app/Day04.hs
@@ -1,20 +1,29 @@
 module Day04 where
 
+import Data.List (inits, tails)
 import qualified Data.ByteString as B
 import qualified Data.ByteString.Char8 as BC
 
 part1 :: String -> String
-part1 (BC.pack -> input) = show . sum $ map (\c -> length (filter (check c) xmases)) coords
+part1 = show . countMatches xmases
+  where
+    xmases = zip "XMAS" <$> rays
+    rays = tail $ zip <$> dirs <*> dirs
+    dirs = [[0,0..], [0,1..],[0,-1..]]
+
+part2 :: String -> String
+part2 = show . countMatches xmases
+  where
+    xmases = (('A', (0, 0)) :) <$> mmsss
+    mmsss = zip "MMSS" <$> rotations [(-1, -1), (1, -1), (1, 1), (-1, 1)]
+    rotations s = init $ zipWith (++) (tails s) (inits s)
+
+type Pattern = [(Char, (Int, Int))]
+
+countMatches :: [Pattern] -> String -> Int
+countMatches pats (BC.pack -> input) = sum $ map (\c -> length (filter (check c) pats)) coords
   where
     check (x, y) = all (\(c, (dx, dy)) -> charAt (x + dx, y + dy) == Just c)
     coords = [(x, y) | y <- [0..n - 1], x <- [0..n - 1]]
-    xmases = zip "XMAS" <$> dirs
-    dirs = tail $ zip <$> [same, inc, dec] <*> [same, inc, dec]
-    inc = [0..]
-    dec = [0,-1..]
-    same = [0,0..]
     charAt (x, y) = BC.indexMaybe input $ y * (n + 1) + x
     Just n = B.elemIndex 10 input
-
-part2 :: String -> String
-part2 _ = "Day 04b not implemented yet"
