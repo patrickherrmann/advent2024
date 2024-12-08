7c92fd046b1fce7221e0722fdf6ab1bb517c0712
Patrick Herrmann <patrick.herrmann@classdojo.com>
2024-12-08 02:16:56 +0000

    Solve day 07 part 2

--- app/Day07.hs
+++ app/Day07.hs
@@ -1,21 +1,27 @@
 module Day07 where
 
 import Parsing
+import Prelude hiding ((||))
 
 part1 :: String -> String
-part1 = show . sum . map fst . filter possible . parseEquations
-  where
-    possible (lhs, x:xs) = any (== lhs) $ values [x] xs
-    values :: [Integer] -> [Integer] -> [Integer]
-    values acc = \case
-      [] -> acc
-      x:xs -> values ( [(*), (+)] <*> acc <*> [x] ) xs
-    
+part1 = show . sum . map fst . filter (possible [(*), (+)]) . parseEquations
 
 part2 :: String -> String
-part2 _ = "Day 07b not implemented yet"
+part2 = show . sum . map fst . filter (possible [(*), (+), (||)]) . parseEquations
+
+(||) :: Op
+a || b = read $ show a ++ show b
 
 type Equation = (Integer, [Integer])
+type Op = Integer -> Integer -> Integer
+
+possible :: [Op] -> Equation -> Bool
+possible ops (lhs, x:xs) = any (== lhs) $ values ops [x] xs
+
+values :: [Op] -> [Integer] -> [Integer] -> [Integer]
+values ops acc = \case
+  [] -> acc
+  x:xs -> values ops (ops <*> acc <*> [x] ) xs
 
 parseEquations :: String -> [Equation]
 parseEquations = parseUnsafe $ equation `sepBy` newline
