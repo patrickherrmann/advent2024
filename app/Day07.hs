c6a777cf1185a319278ab92c0a26567b8ddd1c3b
Patrick Herrmann <patrick.herrmann@classdojo.com>
2024-12-08 02:08:34 +0000

    Solve day 07 part 1

--- app/Day07.hs
+++ app/Day07.hs
@@ -1,7 +1,28 @@
 module Day07 where
 
+import Parsing
+
 part1 :: String -> String
-part1 _ = "Day 07a not implemented yet"
+part1 = show . sum . map fst . filter possible . parseEquations
+  where
+    possible (lhs, x:xs) = any (== lhs) $ values [x] xs
+    values :: [Integer] -> [Integer] -> [Integer]
+    values acc = \case
+      [] -> acc
+      x:xs -> values ( [(*), (+)] <*> acc <*> [x] ) xs
+    
 
 part2 :: String -> String
 part2 _ = "Day 07b not implemented yet"
+
+type Equation = (Integer, [Integer])
+
+parseEquations :: String -> [Equation]
+parseEquations = parseUnsafe $ equation `sepBy` newline
+  where
+    equation = do
+      lhs <- decimal
+      void $ string ": "
+      rhs <- decimal `sepBy` char ' '
+      return (lhs, rhs)
+
