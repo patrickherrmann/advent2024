c8896c615bfdd31a3c751d5c8e618308a1df326e
Patrick Herrmann <patrick.herrmann@classdojo.com>
2024-12-10 11:58:49 +0000

    Solve day 10 part 1

--- app/Day10.hs
+++ app/Day10.hs
@@ -1,7 +1,54 @@
 module Day10 where
 
+import Data.Array.Unboxed
+import Data.Array.ST
+import Data.Char (ord)
+import Control.Monad.ST
+import Control.Monad (forM_, foldM, void)
+
 part1 :: String -> String
-part1 _ = "Day 10a not implemented yet"
+part1 input = show $ sum $ map (scores !) (trailheads g)
+  where
+    g = parseInput input
+    scores = computeScores g
 
 part2 :: String -> String
 part2 _ = "Day 10b not implemented yet"
+
+type Coord = (Int, Int)
+type Height = Int
+type Grid = UArray Coord Height
+type Scores s = STUArray s Coord Int
+
+trailheads :: Grid -> [Coord]
+trailheads g = [c | c <- indices g, g ! c == 0]
+
+computeScores :: Grid -> UArray Coord Int
+computeScores g = runSTUArray $ do
+  scores <- newArray (bounds g) 0
+  forM_ (indices g) $ incScores g scores
+  return scores
+
+incScores :: Grid -> Scores s -> Coord -> ST s ()
+incScores g scores = void . go 9 []
+  where
+    go h v c
+      | inRange (bounds g) c && g ! c == h && not (c `elem` v) = do
+        s <- readArray scores c
+        writeArray scores c (s + 1)
+        let v' = c : v
+        if (h > 0)
+          then foldM (go (h -1)) v' (neighbors c)
+          else return v'
+      | otherwise = return v
+
+
+neighbors :: Coord -> [Coord]
+neighbors (x, y) = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+
+parseInput :: String -> Grid
+parseInput input = listArray ((1, 1), (n, n)) hs
+  where
+    hs = map (\c -> ord c - ord '0') (concat ls)
+    ls = lines input
+    n = length ls
