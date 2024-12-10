4359c4af7bebfaf3bc3af709e50af37ce602a9fa
Patrick Herrmann <patrick.herrmann@classdojo.com>
2024-12-10 03:16:48 +0000

    Finally solve day 6 part 2

--- app/Day06.hs
+++ app/Day06.hs
@@ -1,62 +1,71 @@
-{-# LANGUAGE Strict #-}
-
 module Day06 where
 
-import qualified Data.ByteString as B
+import Data.List (nub)
+import Data.ByteString (ByteString)
 import qualified Data.ByteString.Char8 as BC
-import Data.Set (Set)
-import qualified Data.Set as Set
 
 part1 :: String -> String
-part1 (BC.pack -> input) = show . Set.size . Set.map fst . fst $ sim input Nothing
+part1 = show . length . pathCoords . parseInput
  
 part2 :: String -> String
-part2 (BC.pack -> input) = show . length $ filter (\i -> snd $ sim input (Just i)) $ obstacleIndices
+part2 input = show . length $ filter (loops . states) g's
   where
-    indicesInPath = Set.map (coordToIndex . fst) . fst $ sim input Nothing
-    coordToIndex (x, y) = y * (n + 1) + x
-    obstacleIndices = filter (`Set.member` indicesInPath) $ BC.elemIndices '.' input
-    Just n = BC.elemIndex '\n' input
+    g = parseInput input
+    g's = map (\c -> g { obstacle = Just c }) (tail (pathCoords g))
+
+pathCoords :: Grid -> [Coord]
+pathCoords = nub . map fst . states
+
+states :: Grid -> [(Coord, Dir)]
+states g = go (s0 g)
+  where
+    go (c, d) = case charAt g c of
+      Just '#' -> go (move (opp d) c, turnRight d)
+      Just _ -> (c, d) : go (move d c, d)
+      Nothing -> []
+
+loops :: Eq a => [a] -> Bool
+loops as = go as as
+  where
+    go (t:ts) (_:h:hs) = t == h || go ts hs
+    go _ _ = False
 
 type Coord = (Int, Int)
-data Dir = U | R | D | L deriving (Enum, Bounded, Eq, Ord, Show)
+type Dir = (Int, Int)
 
-nextDir :: Dir -> Dir
-nextDir = \case
-  L -> U
-  d -> succ d
+data Grid = Grid
+  { bytes :: ByteString
+  , size :: Int
+  , c0 :: Coord
+  , obstacle :: Maybe Coord
+  }
 
-offset :: Dir -> Coord
-offset = \case
-  U -> (0, -1)
-  R -> (1, 0)
-  D -> (0, 1)
-  L -> (-1, 0)
-
-type Visited = Set (Coord, Dir)
-
-sim :: B.ByteString -> Maybe Int -> (Visited, Bool)
-sim input obstruction = go Set.empty ((x0, y0), U)
+parseInput :: String -> Grid
+parseInput input = Grid b n (x0, y0) Nothing
   where
-    charAt (x, y)
-      | idx < 0 || idx >= B.length input = Nothing
-      | Just idx == obstruction = Just '#'
-      | otherwise = BC.indexMaybe input idx
-      where
-        idx = y * (n + 1) + x
-    Just n = BC.elemIndex '\n' input
-    Just i0 = BC.elemIndex '^' input
+    b = BC.pack input
+    Just n = BC.elemIndex '\n' b
+    Just i0 = BC.elemIndex '^' b
     (y0, x0) = i0 `divMod` (n + 1)
-    move ((x, y), (offset -> (dx, dy))) = (x + dx, y + dy)
-    go :: Visited -> (Coord, Dir) -> (Visited, Bool)
-    go visited s@((x, y), d)
-      | s `Set.member` visited = (visited, True) -- infinite loop
-      | otherwise =
-        case charAt next of
-          Just '#' -> go nextVisited ((x, y), nextDir d)
-          Just _   -> go nextVisited (next, d)
-          Nothing  -> (nextVisited, False) -- left the grid
-        where
-          next = move s
-          nextVisited = Set.insert s visited
 
+s0 :: Grid -> (Coord, Dir)
+s0 g = (c0 g, (0, -1))
+
+charAt :: Grid -> Coord -> Maybe Char
+charAt g c@(x, y)
+  | Just c == obstacle g = Just '#'
+  | x < 0 || y < 0 || x >= size g || y >= size g = Nothing
+  | otherwise = BC.indexMaybe (bytes g) (x + y * (size g + 1))
+
+move :: Dir -> Coord -> Coord
+move (dx, dy) (x, y) = (x + dx, y + dy)
+
+turnRight :: Dir -> Dir
+turnRight = \case
+  (0, -1) -> (1, 0)
+  (1, 0) -> (0, 1)
+  (0, 1) -> (-1, 0)
+  (-1, 0) -> (0, -1)
+
+opp :: Dir -> Dir
+opp (dx, dy) = (-dx, -dy)
