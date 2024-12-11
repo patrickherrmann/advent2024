1d1089dd2707608a16cd71a1f73b664474adc37f
Patrick Herrmann <patrick.herrmann@classdojo.com>
2024-12-11 18:44:44 +0000

    Solve day 9 part 2

--- app/Day09.hs
+++ app/Day09.hs
@@ -1,10 +1,12 @@
+{-# LANGUAGE OverloadedLists #-}
+
 module Day09 where
 
-import Data.Sequence (Seq, (<|))
+import Data.Sequence (Seq, (<|), pattern (:<|), pattern (:|>), pattern Empty, (|>), (><))
 import qualified Data.Sequence as Seq
 
 part1 :: String -> String
-part1 input = show $ checksum $ take (sum fileSizes) condense
+part1 input = show $ checksum1 $ take (sum fileSizes) condense
   where
     fileContent = concat $ zipWith replicate fileSizes [0..]
     fileSizes = everyOther ints
@@ -14,20 +16,41 @@ part1 input = show $ checksum $ take (sum fileSizes) condense
     go fid (fs:[]) _ = replicate fs fid
     go _ [] _ = []
 
-checksum :: [Int] -> Int
-checksum = sum . zipWith (*) [0..]
+checksum1 :: [Int] -> Int
+checksum1 = sum . zipWith (*) [0..]
 
 everyOther :: [Int] -> [Int]
 everyOther (fs:_:fss) = fs : everyOther fss
 everyOther (fs:[]) = [fs]
 everyOther _ = []
 
-data Block = File Int Int | Space Int deriving (Eq, Show)
-
 part2 :: String -> String
-part2 input = "TODO"
-  where
-    blocks = parseInput input
+part2 = show . checksum2 0 . reformat . parseInput
+
+reformat :: Seq Block -> Seq Block
+reformat = \case
+  Empty -> []
+  beforeSpace :|> Space s -> reformat beforeSpace >-< [Space s]
+  beforeFile :|> File fid fsize -> case findGap fsize beforeFile of
+    (beforeGap, Space s :<| afterGap) -> reformat ((beforeGap |> File fid fsize |> Space (s - fsize)) >-< afterGap) >-< [Space fsize]
+    _ -> reformat beforeFile |> File fid fsize
+
+checksum2 :: Int -> Seq Block -> Int
+checksum2 pos = \case
+  Empty -> 0
+  Space s :<| bs -> checksum2 (pos + s) bs
+  File fid fsize :<| bs -> (sum $ zipWith (*) [pos..] (replicate fsize fid)) + checksum2 (pos + fsize) bs
+
+type Fid = Int
+data Block = File Fid Int | Space Int deriving (Eq, Show)
+
+-- Seq concatenation that melds adjacent spaces
+(>-<) :: Seq Block -> Seq Block -> Seq Block
+(as :|> Space a) >-< (Space b :<| bs) = as >< (Space (a + b) <| bs)
+as >-< bs = as >< bs
+
+findGap :: Int -> Seq Block -> (Seq Block, Seq Block)
+findGap s = Seq.breakl $ \case Space s' -> s' >= s; _ -> False
 
 parseInput :: String -> Seq Block
 parseInput = go 0
@@ -35,5 +58,5 @@ parseInput = go 0
     go :: Int -> String -> Seq Block
     go fid = \case
       (f:s:bs) -> File fid (read [f]) <| Space (read [s]) <| go (fid + 1) bs
-      (f:[]) -> Seq.singleton $ File fid (read [f])
-      [] -> Seq.empty
+      (f:[]) -> [File fid (read [f])]
+      [] -> []
