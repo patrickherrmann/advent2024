556e3193e1184357b9f53139170158b38559fdce
Patrick Herrmann <patrick.herrmann@classdojo.com>
2024-12-11 22:40:26 +0000

    Rework day 9 part 1 to use the same strategy as part 2

--- app/Day09.hs
+++ app/Day09.hs
@@ -6,40 +6,34 @@ import Data.Sequence (Seq, (<|), pattern (:<|), pattern (:|>), pattern Empty, (|
 import qualified Data.Sequence as Seq
 
 part1 :: String -> String
-part1 input = show $ checksum1 $ take (sum fileSizes) condense
-  where
-    fileContent = concat $ zipWith replicate fileSizes [0..]
-    fileSizes = everyOther ints
-    ints = map (read . (: [])) input
-    condense = go 0 ints (reverse fileContent)
-    go fid (fs:ss:is) filler = replicate fs fid ++ take ss filler ++ go (fid + 1) is (drop ss filler)
-    go fid (fs:[]) _ = replicate fs fid
-    go _ [] _ = []
+part1 = show . checksum 0 . reformat1 . parseInput
 
-checksum1 :: [Int] -> Int
-checksum1 = sum . zipWith (*) [0..]
-
-everyOther :: [Int] -> [Int]
-everyOther (fs:_:fss) = fs : everyOther fss
-everyOther (fs:[]) = [fs]
-everyOther _ = []
+reformat1 :: Seq Block -> Seq Block
+reformat1 = \case
+  Empty -> []
+  beforeSpace :|> Space s -> reformat1 beforeSpace >-< [Space s]
+  beforeFile :|> File fid fsize -> case findGap 1 beforeFile of
+    (beforeGap, Space s :<| afterGap) -> if s < fsize
+      then reformat1 ((beforeGap |> File fid s) >-< (afterGap |> File fid (fsize - s))) >-< [Space s]
+      else reformat1 ((beforeGap |> File fid fsize |> Space (s - fsize)) >-< afterGap) >-< [Space fsize]
+    _ -> reformat1 beforeFile |> File fid fsize
 
 part2 :: String -> String
-part2 = show . checksum2 0 . reformat . parseInput
+part2 = show . checksum 0 . reformat2 . parseInput
 
-reformat :: Seq Block -> Seq Block
-reformat = \case
+reformat2 :: Seq Block -> Seq Block
+reformat2 = \case
   Empty -> []
-  beforeSpace :|> Space s -> reformat beforeSpace >-< [Space s]
+  beforeSpace :|> Space s -> reformat2 beforeSpace >-< [Space s]
   beforeFile :|> File fid fsize -> case findGap fsize beforeFile of
-    (beforeGap, Space s :<| afterGap) -> reformat ((beforeGap |> File fid fsize |> Space (s - fsize)) >-< afterGap) >-< [Space fsize]
-    _ -> reformat beforeFile |> File fid fsize
+    (beforeGap, Space s :<| afterGap) -> reformat2 ((beforeGap |> File fid fsize |> Space (s - fsize)) >-< afterGap) >-< [Space fsize]
+    _ -> reformat2 beforeFile |> File fid fsize
 
-checksum2 :: Int -> Seq Block -> Int
-checksum2 pos = \case
+checksum :: Int -> Seq Block -> Int
+checksum pos = \case
   Empty -> 0
-  Space s :<| bs -> checksum2 (pos + s) bs
-  File fid fsize :<| bs -> (sum $ zipWith (*) [pos..] (replicate fsize fid)) + checksum2 (pos + fsize) bs
+  Space s :<| bs -> checksum (pos + s) bs
+  File fid fsize :<| bs -> (sum $ zipWith (*) [pos..] (replicate fsize fid)) + checksum (pos + fsize) bs
 
 type Fid = Int
 data Block = File Fid Int | Space Int deriving (Eq, Show)
