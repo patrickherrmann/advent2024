039b985d4c696c7952d1957235e42bda307c0090
Patrick Herrmann <patrick.herrmann@classdojo.com>
2024-12-11 21:31:22 +0000

    Implement day 11

--- app/Day11.hs
+++ app/Day11.hs
@@ -1,7 +1,30 @@
 module Day11 where
 
+import Data.MemoTrie
+
+type Stone = Integer
+
 part1 :: String -> String
-part1 _ = "Day 11a not implemented yet"
+part1 = show . lengthAfter 25 . parseInput
 
 part2 :: String -> String
-part2 _ = "Day 11b not implemented yet"
+part2 = show . lengthAfter 75 . parseInput
+
+lengthAfter :: Int -> [Stone] -> Int
+lengthAfter n = sum . map (stoneLengthAfter n)
+  where
+    stoneLengthAfter = curry . memo $ \case
+      (0, _) -> 1
+      (n', stone) -> lengthAfter (n' - 1) (blink stone)
+
+blink :: Stone -> [Stone]
+blink = \case
+  0 -> [1]
+  x -> if even n then [read a, read b] else [x * 2024]
+    where
+      s = show x
+      n = length s
+      (a, b) = splitAt (n `div` 2) s
+
+parseInput :: String -> [Stone]
+parseInput = map read . words
