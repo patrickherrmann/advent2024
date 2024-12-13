47211a1029da4eb67bc1d7010ef13d876a4bd59f
Patrick Herrmann <patrick.herrmann@classdojo.com>
2024-12-13 17:34:10 +0000

    Solve part 2

--- app/Day12.hs
+++ app/Day12.hs
@@ -6,12 +6,12 @@ part1 :: String -> String
 part1 = show . sum . map cost . searchGrid . parseInput
 
 part2 :: String -> String
-part2 _ = "Day 12b not implemented yet"
+part2 = show . sum . map discountCost . searchGrid . parseInput
 
 type Coord = (Int, Int)
 type Grid = UArray Coord Char
 
-data Region = Region { label :: Char, perimeter :: Int, searched :: [Coord], members :: [Coord] } deriving (Show)
+data Region = Region { perimeter :: Int, sides :: Int, searched :: [Coord], members :: [Coord] } deriving (Show)
 
 searchGrid :: Grid -> [Region]
 searchGrid g = foldr go [] (indices g)
@@ -21,20 +21,39 @@ searchGrid g = foldr go [] (indices g)
       | otherwise = searchRegion g c : rs
 
 searchRegion :: Grid -> Coord -> Region
-searchRegion g c0 = go c0 (Region (g ! c0) 0 [] [])
+searchRegion g c0 = go c0 c0 (Region 0 0 [] [])
   where
-    go c r
-      | not (inRange (bounds g) c) = r { perimeter = perimeter r + 1 }
+    label = g ! c0
+    outsideRegion c = not (inRange (bounds g) c) ||  g ! c /= label
+    incPerim r = r { perimeter = perimeter r + 1 }
+    incSides parent c r = if sameSide then r else r { sides = sides r + 1 }
+      where
+        (inside, outside) = sideContinuation (parent, c)
+        sameSide = (not . outsideRegion) inside && outsideRegion outside
+    go parent c r
       | c `elem` members r = r
-      | c `elem` searched r = r { perimeter = perimeter r + 1 }
-      | g ! c /= label r = r { perimeter = perimeter r + 1, searched = c : searched r }
-      | otherwise = foldr go (r { searched = c : searched r, members = c : members r }) (neighbors c)
+      | c `elem` searched r = (incPerim . incSides parent c) r
+      | outsideRegion c = ((incPerim . incSides parent c) r) { searched = c : searched r }
+      | otherwise = foldr (go c) (r { searched = c : searched r, members = c : members r }) (neighbors c)
 
 cost :: Region -> Int
 cost r = perimeter r * length (members r)
 
+discountCost :: Region -> Int
+discountCost r = sides r * length (members r)
+
 neighbors :: Coord -> [Coord]
 neighbors (x, y) = [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]
+
+-- Given coords passing from inside to outside of a region,
+-- return the coords immedately to the left of these. If they
+-- are also passing from inside to outside, this pair is a continuation
+-- of an already-counted side.
+sideContinuation :: (Coord, Coord) -> (Coord, Coord)
+sideContinuation ((x, y), (x', y'))
+  | x == x' = let dy = y' - y in ((x + dy, y), (x + dy, y'))
+  | y == y' = let dx = x' - x in ((x, y - dx), (x', y - dx))
+  | otherwise = error "coords must be adjacent"
 
 parseInput :: String -> Grid
 parseInput input = listArray ((1, 1), (n, n)) (concat ls)
