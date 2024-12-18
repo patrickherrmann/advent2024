71d48ad3ebff0bd6d4ac91d9c2b18e2ba9fb2fab
Patrick Herrmann <patrick.herrmann@classdojo.com>
2024-12-18 04:26:01 +0000

    Start work on day 16 part 2

--- app/Day16.hs
+++ app/Day16.hs
@@ -1,7 +1,12 @@
 module Day16 where
 
+
 import Data.Array.Unboxed
 import Data.List (elemIndex)
+import Data.PQueue.Prio.Min (MinPQueue, pattern (:<), pattern Empty)
+import qualified Data.PQueue.Prio.Min as PQ
+import Data.Map (Map)
+import qualified Data.Map as Map
 import Debug.Trace
 
 part1 :: String -> String
@@ -14,22 +19,44 @@ type Coord = (Int, Int)
 type Grid = UArray Coord Char
 type Cost = Int
 
-type State = (Coord, Dir, Cost)
-data Dir = N | E | S | W deriving (Eq, Show)
+type State = (Coord, Dir)
+data Dir = N | E | S | W deriving (Eq, Ord, Show)
+type Lineage = (Cost, [State])
+type Result = Map State Lineage
 
-search :: Grid -> Int
-search g = go [state0 g] []
+search :: Grid -> Result
+search g = go (PQ.singleton 0 s0) [] (Map.singleton s0 (0, []))
   where
-    go [] _ = maxBound
-    go ((c, dir, cost):ss) visited
-      | (c, dir) `elem` visited = go ss visited
-      | otherwise = case g ! c of
-        '#' -> go ss visited
-        'E' -> min cost $ go ss visited
-        _ -> go ([(move dir c, dir, cost + 1)] ++ ss ++ [(c, dir', cost + 1000) | dir' <- turns dir]) ((c, dir) : visited)
+    s0 = (start g, E)
+    go :: MinPQueue Cost State -> [State] -> Result -> Result
+    go q visited result = case q of
+      Empty -> error "No path found"
+      (cost, s@(c, dir)) :< q' -> case g ! c of
+        'E' -> result
+        '#' -> go q' visited result
+        _ -> go q'' visited' result'
+          where
+            visited' = s : visited
+            lineage = getLineage s result
+            ns = filter (not . (`elem` visited) . fst) $ neighbors (s, lineage)
+            (q'', result') = foldl step (q', result) ns
+            step (_q, _r) (ns, nl1@(ncost, nparents)) = (_q', _r')
+              where
+                nl2 = getLineage ns _r
+                _r' = Map.insert ns (merge nl1 nl2) _r
+                _q' = PQ.insert ncost ns _q
 
-state0 :: Grid -> State
-state0 g = (start g, E, 0)
+merge :: Lineage -> Lineage -> Lineage
+merge a@(aCost, aParents) b@(bCost, bParents) = case compare aCost bCost of
+  EQ -> (aCost, aParents ++ bParents)
+  LT -> a
+  GT -> b
+
+neighbors :: (State, Lineage) -> [(State, Lineage)]
+neighbors (s@(c, dir), (cost, parents)) = ((move dir c, dir), (cost + 1, [s])) : [((c, dir'), (cost + 1000, [s])) | dir' <- turns dir]
+
+getLineage :: State -> Result -> Lineage
+getLineage s r = Map.findWithDefault (maxBound, []) s r
 
 move :: Dir -> Coord -> Coord
 move d (y, x) = case d of
@@ -47,6 +74,9 @@ turns = \case
 
 start :: Grid -> Coord
 start g = head [c | c <- indices g, g ! c == 'S']
+
+end :: Grid -> Coord
+end g = head [c | c <- indices g, g ! c == 'E']
 
 parseInput :: String -> Grid
 parseInput s = listArray ((1, 1), (n, n)) $ filter (/= '\n') s
