df8f2fb94942dbbe35067d596ed4f6ce1b2d20ab
Patrick Herrmann <patrick.herrmann@classdojo.com>
2024-12-19 22:48:33 +0000

    Solve day 19 part 2

--- app/Day19.hs
+++ app/Day19.hs
@@ -5,45 +5,45 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 
 part1 :: String -> String
-part1 input = show $ length $ filter (matches dfa) designs
+part1 input = show $ length $ filter (matches trie) designs
   where
     (towels, designs) = parseInput input
-    dfa = combos towels
+    trie = combos towels
 
 part2 :: String -> String
-part2 input = show $ sum $ map (matchCount dfa) designs
+part2 input = show $ sum $ map (matchCount trie) designs
   where
     (towels, designs) = parseInput input
-    dfa = combos towels
+    trie = combos towels
 
-combos :: [String] -> DFA
-combos = star . foldl1 alt . map literal
+combos :: [String] -> Trie
+combos = plus . foldl1 alt . map literal
 
-data DFA = Node Int (Map Char DFA)
+data Trie = Node Int (Map Char Trie)
 
-literal :: String -> DFA
+literal :: String -> Trie
 literal = \case
-  [] -> Node 1 Map.empty
+  "" -> Node 1 Map.empty
   (c:cs) -> Node 0 $ Map.singleton c (literal cs)
 
-alt :: DFA -> DFA -> DFA
-alt (Node a m1) (Node b m2) = Node (a + b) (Map.unionWith alt m1 m2)
+alt :: Trie -> Trie -> Trie
+alt (Node a e1) (Node b e2) = Node (a + b) (Map.unionWith alt e1 e2)
 
-app :: DFA -> DFA -> DFA
-app (Node a edges) other = if a > 0 then alt n' other else n'
-  where n' = Node a $ Map.map (`app` other) edges
+app :: Trie -> Trie -> Trie
+app (Node a e) other = if a > 0 then alt n' other else n'
+  where n' = Node a $ Map.map (`app` other) e
 
-star :: DFA -> DFA
-star dfa = dfa `app` star dfa
+plus :: Trie -> Trie
+plus trie = trie `app` plus trie
 
-matches :: DFA -> String -> Bool
-matches dfa s = matchCount dfa s > 0
+matches :: Trie -> String -> Bool
+matches trie s = matchCount trie s > 0
 
-matchCount :: DFA -> String -> Int
+matchCount :: Trie -> String -> Int
 matchCount (Node a m) = \case
-  [] -> a
+  "" -> a
   (c:cs) -> case Map.lookup c m of
-    Just dfa -> matchCount dfa cs
+    Just trie -> matchCount trie cs
     Nothing -> 0
 
 type Towel = String
