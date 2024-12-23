823afcba62d23da5a0b17462036042c46c1f3e6a
Patrick Herrmann <patrick.herrmann@classdojo.com>
2024-12-23 21:04:49 +0000

    Solve day 23

--- app/Day23.hs
+++ app/Day23.hs
@@ -1,33 +1,53 @@
 module Day23 where
 
-import Data.Map (Map)
+import Data.Map (Map, (!))
 import qualified Data.Map as Map
+import Data.Ord (comparing)
 import Data.List
 
 part1 :: String -> String
-part1 = show . length . lans . graph . bidir . parseInput
+part1 = show . length . lans . graph . parseInput
 
 part2 :: String -> String
-part2 _ = "Day 23b not implemented yet"
+part2 = password . maximumBy (comparing length) . maximalCliques . graph . parseInput
 
-lans :: Map String [String] -> [[String]]
-lans g = nub . map sort $ go (Map.assocs g)
+password :: [Vertex] -> String
+password = intercalate "," . sort
+
+maximalCliques :: Graph -> [[Vertex]]
+maximalCliques g = foldr add [] (Map.keys g)
   where
-    go ((a, a's):xs) 
-      | head a == 't' = [[a, b, c] | (b, c) <- pairs a's, b `elem` g Map.! c] ++ go xs
-      | otherwise = go xs
-    go [] = []
+    add x cliques
+      | any (x `elem`) cliques = cliques
+      | otherwise = maximalClique g x : cliques
 
-graph :: [(String, String)] -> Map String [String]
-graph = Map.fromListWith (++) . map (\(a, b) -> (a, [b]))
+maximalClique :: Graph -> Vertex -> [Vertex]
+maximalClique g v = foldr invite [v] (Map.keys g)
+  where
+    invite x clique
+      | x == v = clique
+      | all (`elem` (g ! x)) clique = x : clique
+      | otherwise = clique
 
-bidir :: [(String, String)] -> [(String, String)]
-bidir ((a, b):xs) = (a, b) : (b, a) : bidir xs
-bidir [] = []
+lans :: Graph -> [[Vertex]]
+lans g = nub . map sort $ Map.assocs g >>= \case
+  (a, vs) | head a == 't' -> [[a, b, c] | (b, c) <- pairs vs, b `elem` g ! c]
+  _ -> []
+
+type Graph = Map String [String]
+type Vertex = String
+type Edge = (String, String)
+
+graph :: [Edge] -> Graph
+graph = Map.fromListWith (++) . map (\(a, b) -> (a, [b])) . undirected
+
+undirected :: [Edge] -> [Edge]
+undirected = (>>= \(a, b) -> [(a, b), (b, a)])
 
 pairs :: [a] -> [(a, a)]
-pairs (x:xs) = map (x,) xs ++ pairs xs
-pairs [] = []
+pairs = \case
+  x:xs -> map (x,) xs ++ pairs xs
+  [] -> []
 
-parseInput :: String -> [(String, String)]
+parseInput :: String -> [Edge]
 parseInput = map (\l -> (take 2 l, drop 3 l)) . lines
