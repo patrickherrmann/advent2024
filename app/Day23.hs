9b3608cb080bd877be01f7ebc55aded6389249f3
Patrick Herrmann <patrick.herrmann@classdojo.com>
2024-12-23 18:31:46 +0000

    Solve day 23 part 1

--- app/Day23.hs
+++ app/Day23.hs
@@ -1,7 +1,33 @@
 module Day23 where
 
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.List
+
 part1 :: String -> String
-part1 _ = "Day 23a not implemented yet"
+part1 = show . length . lans . graph . bidir . parseInput
 
 part2 :: String -> String
 part2 _ = "Day 23b not implemented yet"
+
+lans :: Map String [String] -> [[String]]
+lans g = nub . map sort $ go (Map.assocs g)
+  where
+    go ((a, a's):xs) 
+      | head a == 't' = [[a, b, c] | (b, c) <- pairs a's, b `elem` g Map.! c] ++ go xs
+      | otherwise = go xs
+    go [] = []
+
+graph :: [(String, String)] -> Map String [String]
+graph = Map.fromListWith (++) . map (\(a, b) -> (a, [b]))
+
+bidir :: [(String, String)] -> [(String, String)]
+bidir ((a, b):xs) = (a, b) : (b, a) : bidir xs
+bidir [] = []
+
+pairs :: [a] -> [(a, a)]
+pairs (x:xs) = map (x,) xs ++ pairs xs
+pairs [] = []
+
+parseInput :: String -> [(String, String)]
+parseInput = map (\l -> (take 2 l, drop 3 l)) . lines
