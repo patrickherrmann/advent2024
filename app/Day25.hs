6a0865f94cd20541505abe45d7033138a2372839
Patrick Herrmann <patrick.herrmann@classdojo.com>
2024-12-25 05:39:25 +0000

    Solve day 25

--- app/Day25.hs
+++ app/Day25.hs
@@ -1,7 +1,27 @@
 module Day25 where
 
+import Data.List
+import Data.List.Split
+
 part1 :: String -> String
-part1 _ = "Day 25a not implemented yet"
+part1 = show . length . compatible . parseInput
 
 part2 :: String -> String
-part2 _ = "Day 25b not implemented yet"
+part2 = const "Merry Christmas!"
+
+compatible :: ([Key], [Lock]) -> [(Key, Lock)]
+compatible (keys, locks) = [(key, lock) | key <- keys, lock <- locks, fits key lock]
+
+fits :: Key -> Lock -> Bool
+fits key lock = all (<8) $ zipWith (+) key lock
+
+type Key = [Int]
+type Lock = [Int]
+
+parseInput :: String -> ([Key], [Lock])
+parseInput input = (map counts keyGrids, map counts lockGrids)
+  where
+    grids = splitOn "\n\n" input
+    (keyGrids, lockGrids) = partition isKey grids
+    counts = map (length . filter (== '#')) . transpose . lines
+    isKey g = head g == '.'
