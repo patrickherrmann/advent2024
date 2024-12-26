1b4c2f1a57ac98fe44345b444bbb3e81b58854d8
Patrick Herrmann <patrick.herrmann@classdojo.com>
2024-12-26 05:50:01 +0000

    More performant solution to day 19

--- app/Day19.hs
+++ app/Day19.hs
@@ -1,50 +1,30 @@
 module Day19 where
 
 import Parsing
-import Data.Map (Map)
-import qualified Data.Map as Map
+import Data.List
+import Data.MemoTrie
 
 part1 :: String -> String
-part1 input = show $ length $ filter (matches trie) designs
+part1 input = show $ length $ filter (matches towels) designs
   where
     (towels, designs) = parseInput input
-    trie = combos towels
 
 part2 :: String -> String
-part2 input = show $ sum $ map (matchCount trie) designs
+part2 input = show total
   where
+    total = sum $ map (matches towels) designs :: Int
     (towels, designs) = parseInput input
-    trie = combos towels
 
-combos :: [String] -> Trie
-combos = plus . foldl1 alt . map literal
+class MatchRes r where success :: r; rollup :: [r] -> r
+instance MatchRes Bool where success = True; rollup = any id
+instance MatchRes Int where success = 1; rollup = sum
 
-data Trie = Node Int (Map Char Trie)
-
-literal :: String -> Trie
-literal = \case
-  "" -> Node 1 Map.empty
-  (c:cs) -> Node 0 $ Map.singleton c (literal cs)
-
-alt :: Trie -> Trie -> Trie
-alt (Node a e1) (Node b e2) = Node (a + b) (Map.unionWith alt e1 e2)
-
-app :: Trie -> Trie -> Trie
-app (Node a e) other = if a > 0 then alt n' other else n'
-  where n' = Node a $ Map.map (`app` other) e
-
-plus :: Trie -> Trie
-plus trie = trie `app` plus trie
-
-matches :: Trie -> String -> Bool
-matches trie s = matchCount trie s > 0
-
-matchCount :: Trie -> String -> Int
-matchCount (Node a m) = \case
-  "" -> a
-  (c:cs) -> case Map.lookup c m of
-    Just trie -> matchCount trie cs
-    Nothing -> 0
+matches :: MatchRes m => [Towel] -> Design -> m
+matches towels = go
+  where
+    go = memo $ \case
+      "" -> success
+      design -> rollup [go (drop (length t) design) | t <- towels, t `isPrefixOf` design]
 
 type Towel = String
 type Design = String
@@ -55,3 +35,4 @@ parseInput = parseUnsafe $ do
   void $ newline *> newline
   designs <- many letterChar `sepBy` newline
   return (towels, designs)
+
